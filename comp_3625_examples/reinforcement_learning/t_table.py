5e130ee86c49ba75c3dd8ac62bf0ecbc874ab2fb
echalmers <echalmers@mtroyal.ca>
2025-11-19 14:53:37 -0700

    t and r table skeletons

---
+++ comp_3625_examples/reinforcement_learning/t_table.py
@@ -0,0 +1,22 @@
+
+
+class Ttable:
+
+    def __init__(self):
+        self.counts = dict()
+    
+    def get_transition(self, state, action) -> dict:
+        """
+        returns a dictionary mapping next_state -> probabilities
+        """
+        pass
+
+    def update(self, state, action, new_state) -> None:
+        """
+        update the internal counts
+        """
+        pass
+    
+    def get_all_states(self) -> list:
+        """ return list of all known states """
+        pass
