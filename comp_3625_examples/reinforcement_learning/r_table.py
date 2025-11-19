d8567c819275ba0ecefd61c24e79e2ec51416c03
EthanSigfussonMRU <113393936+EthanSigfussonMRU@users.noreply.github.com>
2025-11-19 15:22:39 -0700

    Add reward updating functionality to update method
    Implement reward updating logic in the update method.

--- comp_3625_examples/reinforcement_learning/r_table.py
+++ comp_3625_examples/reinforcement_learning/r_table.py
@@ -7,7 +7,14 @@ class Rtable:
     
     def update(self, state, action, new_state, reward) -> None:
         """ updates internal rewards dict to reflect this new information """
-        pass
+        key = state, action, new_state
+
+        if key not in self.rewards:
+            self.rewards[key] = (reward, 1)
+        else:
+            self.rewards[key][1] += 1
+            self.rewards[key] = self.rewards[key] + ( reward - self.rewards[key]) / self.transtion_count
+
 
     def get_reward(self, state, action, new_state) -> float:
         """ return average reward for given transition """
